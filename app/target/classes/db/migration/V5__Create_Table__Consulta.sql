d08d4e0ab602bdeb7f5ad95a494b3deb1ad2e397
Luis Felipe Rodrigues <luisfelipebr1995@gmail.com>
2023-05-18 21:29:38 -0300

    Fixing Two

--- app/target/classes/db/migration/V5__Create_Table__Consulta.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE tb_consulta (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    data TIMESTAMP NOT NULL,
-    medico_id BIGINT NOT NULL,
-    paciente_id BIGINT NOT NULL,
-    FOREIGN KEY (medico_id) REFERENCES tb_medico(id),
-    FOREIGN KEY (paciente_id) REFERENCES tb_paciente(id)
-);
--- app/target/classes/db/migration/V6__Populate__Consulta.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `tb_consulta` (id, data, medico_id, paciente_id)
-VALUES (1, '2022-03-15 10:30:00', 1, 2);
